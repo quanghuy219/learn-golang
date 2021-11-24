279082631696ad4c7813f0c3128b83454af1cc16
Huy Pham <p.q.huy97@gmail.com>
2021-11-24 16:28:06 +0700

    Reader Writer interface

--- http/main.go
+++ http/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct {}
 
 func main()  {
 	resp, err := http.Get("http://google.com")
@@ -12,5 +15,15 @@ func main()  {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
-	fmt.Println(resp.Status)
+	// bs := make([]byte, 999999)
+	// resp.Body.Read(bs)
+	// fmt.Println(string(bs))
+
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	return len(bs), nil
 }
